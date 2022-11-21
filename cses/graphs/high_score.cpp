1d45747d1e8ecfa4e8c7f0898dc4beea97bb73dc
periaraperi <periaraperi@protonmail.com>
2022-11-21 23:37:47 +0400

    bellman-ford understood

--- cses/graphs/high_score.cpp
+++ cses/graphs/high_score.cpp
@@ -7,69 +7,40 @@ struct Edge {
     long long w;
 };
 
-// check if last node is reachable from v
-void dfs(int v, const vector<vector<pair<int,long long>>>& graph, vector<bool>& vis)
-{
-    vis[v] = true;
-    for (auto u:graph[v]) {
-        if (!vis[u.first]) 
-            dfs(u.first,graph,vis);
-    }
-}
-
-bool dfs_cycle(int v, long long sm, const vector<vector<pair<int,long long>>>& graph, vector<int>& color)
-{
-    color[v] = 1;
-    for (auto u:graph[v]) {
-        if (color[u.first]==0) {
-            if (dfs_cycle(u.first,sm+u.second,graph,color)) {
-                return true;
-            }
-        } else if (color[u.first]==1) {
-            if (sm+u.second>0) { // positive cycle found
-                // check if endpoint is reachable from this cycle
-                vector<bool> vis(graph.size(),false);
-                dfs(v,graph,vis);
-                if (vis[(int)graph.size()-1] && color[0]==1) return true;
-            }
-        }
-    }
-    
-    color[v] = 2;
-    return false;
-}
-
-
-bool positive_cycle(const vector<vector<pair<int,long long>>>& graph)
-{
-    int n = (int) graph.size();
-    for (int i=0; i<n; ++i) {
-        vector<int> color(n,0);
-        if (dfs_cycle(i,0,graph,color)) return true;
-    }
-    return false; 
-}
-
 long long bellman_ford(const vector<Edge>& edges, int n, int m)
 {
     const long long INF = numeric_limits<long long>::max();
     vector<long long> dis(n,INF); 
-    dis[n-1] = 0;
+    dis[0] = 0;
     
-    for (;;) {
-        bool changed = false;
+    for (int j=0; j<n-1; ++j) { // at most n-1 iterations needed
+        bool changed = 0;
         for (int i=0; i<m; ++i) {
             if (dis[edges[i].a]<INF) {
                 if (dis[edges[i].a]+edges[i].w<dis[edges[i].b]) {
                     dis[edges[i].b] = dis[edges[i].a]+edges[i].w;
-                    changed = true;
+                    changed = true; // at least one change is made
                 }
             }
         }
-        if (!changed)
-            break;
+        if (!changed) { // already have best answers, no need for rest iterations
+            return -dis[n-1];
+        }
     }
-    return dis[0];
+    for (int j=0; j<n-1; ++j) {
+        for (int i=0; i<m; ++i) { // check for negative cycle
+            if (dis[edges[i].a]==numeric_limits<long long>::min()) {
+                dis[edges[i].b] = numeric_limits<long long>::min();
+                continue;
+            }
+            if (dis[edges[i].a]<INF) {
+                if (dis[edges[i].a]+edges[i].w<dis[edges[i].b]) {
+                    dis[edges[i].b] = numeric_limits<long long>::min();
+                }
+            }
+        }
+    }
+    return (dis[n-1]==numeric_limits<long long>::min()) ? -1 : -dis[n-1];
 }
 
 int main() 
@@ -83,21 +54,10 @@ int main()
         cin >> edges[i].a >> edges[i].b >> edges[i].w;
         --edges[i].a;
         --edges[i].b;
-    }
-    vector<vector<pair<int,long long>>> graph(n,vector<pair<int,long long>>());
-    for (auto& e:edges) {
-        graph[e.a].push_back({e.b,e.w});
+        edges[i].w *= (-1);
     }
 
-    if (positive_cycle(graph))
-        cout << -1 << '\n';
-    else {
-        for (auto &e:edges) {
-            swap(e.a,e.b);
-            e.w *= (-1);
-        }
-        cout << -bellman_ford(edges,n,m);
-    }
+    cout << bellman_ford(edges,n,m);
       
     return 0;
 }
