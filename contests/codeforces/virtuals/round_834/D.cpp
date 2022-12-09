09ca1165d2a2162cd23ae25f382493c76574b267
Periaraperi <periaraperi@protonmail.com>
2022-12-10 01:23:25 +0400

    upsolved D. Cute math problem, went in completely different direction.

--- contests/codeforces/virtuals/round_834/D.cpp
+++ contests/codeforces/virtuals/round_834/D.cpp
@@ -2,68 +2,41 @@
 
 using namespace std;
 
-int get_zeros(long long x) 
-{
-    int zero = 0;
-    while (x>0) {
-        if (x%10==0) ++zero;
-        else break;
-        x /= 10;
-    }
-    return zero;
-}
-
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     
-    // pre compute
-    vector<vector<int>> vec(10);
-    vec[1] = {10};
-    vec[2] = {5,10};
-    vec[3] = {10};
-    vec[4] = {5,10}; 
-    vec[5] = {2,4,6,8,10}; 
-    vec[6] = {5,10}; 
-    vec[7] = {10};
-    vec[8] = {5,10}; 
-    vec[9] = {10};
-
     int t = 1; cin >> t;
     while (t--) {
         long long n,m; cin >> n >> m;
-        long long nn = n;
-
-        int zeros_to_add = 0;
-        while (n%10==0) {
-            ++zeros_to_add;
-            n/=10;
+        auto nn = n;
+        int c2 = 0;
+        int c5 = 0;
+        while (nn>0 && nn%2==0) {
+            nn /= 2;
+            ++c2;
         }
-
-        long long ans = 0;
-        int last_zeros = zeros_to_add;
-        int k = n%10;
-        for (auto& a:vec[k]) {
-            long long b = m;
-            while (b%a!=0) --b;
-            for (int i=0; i<10000; ++i) {
-                auto new_price = n*(b);
-                auto num_trailing_zeros = get_zeros(new_price) + zeros_to_add;
-                if (num_trailing_zeros>last_zeros) {
-                    ans = new_price;
-                    last_zeros = num_trailing_zeros;
-                } else if (num_trailing_zeros==last_zeros) {
-                    ans = max(ans,new_price);
-                }
-                b -= a;
-                if (b<=0) break;
-            }
+        while (nn>0 && nn%5==0) {
+            nn /= 5;
+            ++c5;
         }
-        //cout << "asd" << ans <<'\n';
-        for (int i=0; i<zeros_to_add; ++i) ans *= 10;
-        cout << ((ans==0) ? nn*m:ans) << '\n';
+        long long k = 1;
+        while (c2<c5 && 2*k<=m) {
+            ++c2;
+            k *= 2;
+        }
+        while (c5<c2 && 5*k<=m) {
+            ++c5;
+            k *= 5;
+        }
+        while (c5==c2 && 10*k<=m) {
+            ++c5;
+            ++c2;
+            k *= 10;
+        }
+        cout << n*k*(m/k) << '\n';
+        
     }
 
     return 0;
