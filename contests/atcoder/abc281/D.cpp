c9ab5a1737366ff52b4f0fc2ac0cb2aa97c948b3
Periaraperi <periaraperi@protonmail.com>
2022-12-11 13:06:17 +0400

    kinda still don't get it

---
+++ contests/atcoder/abc281/D.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Nice problem
+    Similar to knapsack
+    with some modifications
+    Learned something new :)
+*/
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1; //cin >> t;
+    while (t--) {
+        int n,k,d; cin >> n >> k >> d;
+        vector<long long> v(n);
+        for (auto& i:v) cin >> i;
+        
+        auto INF = numeric_limits<long long>::min();
+        vector<vector<vector<long long>>> dp(n+1,vector<vector<long long>>(k+1,vector<long long>(d,-INF)));
+        dp[0][0][0] = 0;
+
+        for (int i=1; i<=n; ++i) {
+            for (int kk=0; kk<=min(i,k); ++kk) {
+                for (int rem=0; rem<d; ++rem) {
+                    dp[i][kk][rem] = dp[i-1][kk][rem];
+                    if (kk>0)
+                        dp[i][kk][rem] = max(dp[i][kk][rem],
+                                             dp[i-1][kk-1][((rem-(v[i-1]%d))+d)%d]+v[i-1]);
+                }
+            }
+        }
+        if (dp[n][k][0]<0) cout << "-1\n";
+        else               cout << dp[n][k][0] << '\n';
+    }
+    return 0;
+}
