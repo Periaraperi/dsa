66cf1d7f127abaf533d9851d8051c8b1e0698d22
Periaraperi <periaraperi@protonmail.com>
2022-12-13 21:25:07 +0400

    solved be efficiently, for some reason brute-force-ish solution passed (KEK)

--- contests/codeforces/virtuals/edu_139/B.cpp
+++ contests/codeforces/virtuals/edu_139/B.cpp
@@ -11,12 +11,17 @@ int main()
     while (tt--) {
         int n; cin >> n;
         string s; cin >> s;
+        map<string,int> mp;
         bool ok = false;
         for (int i=0; i<n-1; ++i) {
             string tmp = ""; tmp += s[i]; tmp += s[i+1];
-            if (i+2<n && s.find(tmp,i+2)!=string::npos) {
-                ok = true;
-                break;
+            if (mp.find(tmp)!=mp.end()) {
+                if (mp[tmp]<i) {
+                    ok = true;
+                    break;
+                }
+            } else {
+                mp[tmp] = i+1;
             }
         }
         cout << (ok?"YES\n":"NO\n");
