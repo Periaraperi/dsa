c9dde574ef357e642f06b3844978bab47b4539ad
periaraperi <periaraperi@protonmail.com>
2023-04-03 18:06:07 +0400

    solved with graphs and cycles, could not get editorial version, interesting observation that at most 1 cycle in component, finding 1 cycle is easy xD, problem very cool, but many little details, kinda fucked my mind

--- contests/atcoder/abc296/E.cpp
+++ contests/atcoder/abc296/E.cpp
@@ -51,7 +51,6 @@ int main()
 
     while(t--) {
         int n; cin >> n; 
-        vector<int> v(n+1);
         vector<int> fix(n+1,-1);
         vector<vector<int>> graph(n+1);
         for (int i=1; i<=n; ++i) {
