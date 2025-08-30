555a3b9099050e3017c9023f248829390ae753ce
periaraperi <periaraperi@protonmail.com>
2025-08-30 17:57:07 +0400

    updated impl

--- atcoder/abc/421/C.cpp
+++ atcoder/abc/421/C.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <set>
 #include <string>
-#include <numeric>
+#include <limits>
  
 using namespace std;
  
@@ -14,23 +14,7 @@ int main()
     int n; cin >> n;
     string str; cin >> str;
 
-    int64_t final_ans {numeric_limits<int64_t>::max()};
-    for (int k{}; k<4; ++k) {
-        auto s {str};
-        int64_t ans {};
-        if (k == 1) {
-            reverse(s.begin(), s.end());
-        }
-        else if (k == 2) {
-            swap(s[0], s[1]);
-            ++ans;
-        }
-        else if (k == 3) {
-            reverse(s.begin(), s.end());
-            swap(s[0], s[1]);
-            ++ans;
-        }
-
+    auto f = [n](string s) {
         set<int> a; 
         set<int> b; 
         
@@ -38,23 +22,40 @@ int main()
             if (s[i] == 'A') a.insert(i);
             else b.insert(i);
         }
+        
+        set<int>::iterator it;
+        int64_t ans{};
         for (int i{1}; i<2*n; ++i) {
             if (s[i] != s[i-1]) continue;
+
+            int j{};
             if (s[i] == 'A') {
-                auto it {b.lower_bound(i)};
-                swap(s[i], s[*it]);
-                ans += (*it - i);
-                b.erase(*it);
+                it = b.lower_bound(i);
+                j = *it;
+                b.erase(it);
             }
             else {
-                auto it {a.lower_bound(i)};
-                swap(s[i], s[*it]);
-                ans += (*it - i);
-                a.erase(*it);
+                it = a.lower_bound(i);
+                j = *it;
+                a.erase(it);
             }
+            swap(s[i], s[j]);
+            ans += (j - i);
         }
-        final_ans = min(final_ans, ans);
-    }
+        return ans;
+    };
+
+    int64_t final_ans {numeric_limits<int64_t>::max()};
+    auto s2 {str}; 
+
+    final_ans = min(final_ans, f(str));
+    swap(str[0], str[1]);
+    final_ans = min(final_ans, f(str)+1);
+
+    reverse(s2.begin(), s2.end());
+    final_ans = min(final_ans, f(s2));
+    swap(s2[0], s2[1]);
+    final_ans = min(final_ans, f(s2)+1);
 
     cout << final_ans << '\n';
     return 0;
