59555ee20e253a53da9f9808802f255922cfd67b
periaraperi <periaraperi@protonmail.com>
2025-10-13 01:39:38 +0400

    rewrote this with custom modint as well.

--- atcoder/abc/414/E.cpp
+++ atcoder/abc/414/E.cpp
@@ -2,19 +2,104 @@
  
 using namespace std;
 
-int64_t bin_pow(int64_t a, int64_t b, int64_t m)
+namespace peria {
+
+template<int M>
+struct modint;
+
+using mint = peria::modint<998244353>;
+
+template<int M>
+[[nodiscard]] constexpr modint<M> bin_pow(modint<M> a, int64_t b)
 {
-    int64_t res {1};
-    while (b > 0) {
-        if (b%2 == 1) {
-            res *= a;
-            res %= m;
-        }
-        a *= a;
-        a %= m;
+    modint<M> res{1};
+    while (b>0) {
+        if (b%2 == 1) res *= a;
+        a = a*a;
         b /= 2;
     }
-    return res%m;
+    return res;
+}
+
+template<int M>
+struct modint {
+    static constexpr int MOD = M;
+    constexpr modint() noexcept :value_{0} {}
+    constexpr explicit modint(int64_t x) noexcept :value_{static_cast<int>(((x%M)+M)%M)} {}
+
+    [[nodiscard]] constexpr modint operator-() const noexcept
+    { return modint{-value_}; }
+
+    [[nodiscard]] constexpr modint operator+(const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(value_)+static_cast<int64_t>(rhs.value_))%M}; }
+
+    [[nodiscard]] constexpr modint operator-(const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(value_)-static_cast<int64_t>(rhs.value_)+M)%M}; }
+
+    [[nodiscard]] constexpr modint operator*(const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(value_)*static_cast<int64_t>(rhs.value_))%M}; }
+
+    constexpr modint& operator+=(const modint& rhs) noexcept
+    { value_ = (static_cast<int64_t>(value_)+static_cast<int64_t>(rhs.value_))%M; return *this; }
+
+    constexpr modint& operator+=(int64_t x) noexcept
+    { auto y {modint<M>{x}}; return (*this+=y); }
+
+    constexpr modint& operator-=(const modint& rhs) noexcept
+    { value_ = (static_cast<int64_t>(value_)-static_cast<int64_t>(rhs.value_)+M)%M; return *this; }
+
+    constexpr modint& operator-=(int64_t x) noexcept
+    { auto y {modint<M>{x}}; return (*this-=y); }
+
+    constexpr modint& operator*=(const modint& rhs) noexcept
+    { value_ = (static_cast<int64_t>(value_)*static_cast<int64_t>(rhs.value_))%M; return *this; }
+
+    constexpr modint& operator*=(int64_t x) noexcept
+    { auto y {modint<M>{x}}; return (*this*=y); }
+
+    [[nodiscard]] friend constexpr modint operator+(const modint& lhs, const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(lhs.value_)+static_cast<int64_t>(rhs.value_))%M}; }
+
+    [[nodiscard]] friend constexpr modint operator+(int64_t x, const modint& rhs) noexcept
+    { return modint{x}+rhs; }
+
+    [[nodiscard]] friend constexpr modint operator+(const modint& lhs, int64_t x) noexcept
+    { return lhs+modint{x}; }
+
+    [[nodiscard]] friend constexpr modint operator-(const modint& lhs, const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(lhs.value_)-static_cast<int64_t>(rhs.value_)+M)%M}; }
+
+    [[nodiscard]] friend constexpr modint operator-(int64_t x, const modint& rhs) noexcept
+    { return modint{x}-rhs; }
+
+    [[nodiscard]] friend constexpr modint operator-(const modint& lhs, int64_t x) noexcept
+    { return lhs-modint{x}; }
+
+    [[nodiscard]] friend constexpr modint operator*(const modint& lhs, const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(lhs.value_)*static_cast<int64_t>(rhs.value_))%M}; }
+
+    [[nodiscard]] friend constexpr modint operator*(int64_t x, const modint& rhs) noexcept
+    { return modint{x}*rhs; }
+
+    [[nodiscard]] friend constexpr modint operator*(const modint& lhs, int64_t x) noexcept
+    { return lhs*modint{x}; }
+
+    [[nodiscard]] friend constexpr bool operator==(const modint& lhs, const modint& rhs) noexcept
+    { return lhs.value_ == rhs.value_; }
+
+    [[nodiscard]] friend constexpr bool operator!=(const modint& lhs, const modint& rhs) noexcept
+    { return !(lhs == rhs); }
+
+    [[nodiscard]] constexpr int value() const noexcept 
+    { return value_; }
+
+    [[nodiscard]] constexpr modint inverse() noexcept
+    { return bin_pow(*this, M-2); }
+
+private:
+    int value_ {};
+};
+
 }
  
 int main() 
@@ -22,25 +107,21 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    constexpr int64_t MOD {998244353};
-    const auto mod_inverse_two {bin_pow(2, MOD-2, MOD)};
+    constexpr auto mod_inverse_two {peria::mint{2}.inverse()};
 
     int64_t N; cin >> N;
-    auto n {N%MOD};
-    int64_t ans {(((n*(n-1)%MOD)*mod_inverse_two)%MOD + N)%MOD}; // all pairs where a>b + pairs where a == b
+    auto n {peria::mint{N}};
+    auto ans {(n*(n-1))*mod_inverse_two + n}; // all pairs where a>b + pairs where a == b
 
     int64_t i {1};
     while (i<=N) {
         const auto q {N/i};
         const auto j {N/q};
-        auto bad_pairs {(((j-i+1)%MOD)*(q%MOD))%MOD}; // bad pairs where b divides a
+        const auto bad_pairs {peria::mint{j-i+1}*q}; // bad pairs where b divides a
         ans -= bad_pairs;
-        if (ans < 0) ans += MOD;
-        ans %= MOD;
         i = j+1;
     }
-
-    cout << ans << '\n';
+    cout << ans.value() << '\n';
     return 0;
 }
 
