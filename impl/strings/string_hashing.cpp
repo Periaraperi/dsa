2c140c375af588ff49a72d522c6035c905628b74
periaraperi <periaraperi@protonmail.com>
2025-11-03 21:11:22 +0400

    Used my modint struct. Separated powers from hash calculation. Instead of storing hash of entire string, let's store prefix hashes to be able to calculate sub-hashes in O(1) (similar to prefix sums). Each string will have its own "rolling_hash" struct, which will use specific "rolling_hash_powers" struct that we supply. Maybe we want different powers and mod for different strings. Anyhow need to test if implementation is correct.

--- impl/strings/string_hashing.cpp
+++ impl/strings/string_hashing.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
- 
-using namespace std;
+#include <utility>
 
 /*
-
 H(s) = s[0]*p^0 + s[1]*p^1 + s[2]*p^2 + s[3]*p^3 + ... + s[n-1]*p^(n-1)
      = SUM (s[i]*p^i) where 0<=i<=s.len-1
      since powers of p and sum may be very big, compute everything modulo M.
@@ -21,64 +19,198 @@ We want to minimize hash collisions, so take M as a large number.
 In practice, p and M are usually prime numbers, because they give better results.
 p is commonly around the size of alphabet
 
+use ascii values for characters for now.
+
+Some use cases:
+- We can use string hashing to compare 2 strings in O(1).
+- We can check number of duplicate strings in a list of strings.
+- We can calculate any substring hash in O(1) with similar idea as prefix_sums.
+  But this approach requires division with modular arithmetic, so we need to calculate mod inverses
+  for powers. We can precompute this during rolling_hash struct initialization.
+  Additionally there is a trick to prevent mod inverses.
+- We can find number of distinct sub-strings in a string.
+- We can use rolling hashing approach to find occurences of pattern in a string.
+  Rabin-Karp is popular algorithm which implements this approach.
+
+notes:
+- to minimize collisions, especially when we compare many strings with each other, we can use second hash for each string
+  with different P and/or Mod values.
 */
 
-// use ascii values for characters for now.
-struct rolling_hash {
-    rolling_hash(std::size_t max_len, int power, int64_t m_)
-        :p(max_len+1), m{m_}
-    {
-        p[0] = 1LL;
-        for (std::size_t i{1}; i<=max_len; ++i) {
-            p[i] = (p[i-1]*power)%m;
-        }
+namespace peria {
+
+template<int M>
+struct modint;
+
+using mint = modint<1000000009>;
+
+template<int M>
+[[nodiscard]] constexpr modint<M> bin_pow(modint<M> a, int64_t b)
+{
+    modint<M> res{1};
+    while (b>0) {
+        if (b%2 == 1) res *= a;
+        a = a*a;
+        b /= 2;
     }
+    return res;
+}
 
-    [[nodiscard]]
-    static int char_value(unsigned char character) noexcept
-    { return static_cast<int>(character)+1; }
+template<int M>
+struct modint {
+    static constexpr int MOD = M;
+    constexpr modint() noexcept :value_{0} {}
+    constexpr explicit modint(int64_t x) noexcept :value_{static_cast<int>(((x%M)+M)%M)} {}
 
-    [[nodiscard]]
-    int64_t compute_hash(const std::string& s) const noexcept 
-    {
-        // assert that we are only working with strings up to length = powers.size()
-        if (s.size() > p.size()) return -1;
+    [[nodiscard]] constexpr modint operator-() const noexcept
+    { return modint{-value_}; }
 
-        int64_t hash {};
-        for (std::size_t i{}; i<s.size(); ++i) {
-            hash = (hash + p[i]*char_value(s[i]))%m;
-        }
-        return hash;
-    }
+    [[nodiscard]] constexpr modint operator+(const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(value_)+static_cast<int64_t>(rhs.value_))%M}; }
+
+    [[nodiscard]] constexpr modint operator-(const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(value_)-static_cast<int64_t>(rhs.value_)+M)%M}; }
+
+    [[nodiscard]] constexpr modint operator*(const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(value_)*static_cast<int64_t>(rhs.value_))%M}; }
+
+    constexpr modint& operator+=(const modint& rhs) noexcept
+    { value_ = (static_cast<int64_t>(value_)+static_cast<int64_t>(rhs.value_))%M; return *this; }
+
+    constexpr modint& operator+=(int64_t x) noexcept
+    { auto y {modint<M>{x}}; return (*this+=y); }
+
+    constexpr modint& operator-=(const modint& rhs) noexcept
+    { value_ = (static_cast<int64_t>(value_)-static_cast<int64_t>(rhs.value_)+M)%M; return *this; }
+
+    constexpr modint& operator-=(int64_t x) noexcept
+    { auto y {modint<M>{x}}; return (*this-=y); }
+
+    constexpr modint& operator*=(const modint& rhs) noexcept
+    { value_ = (static_cast<int64_t>(value_)*static_cast<int64_t>(rhs.value_))%M; return *this; }
+
+    constexpr modint& operator*=(int64_t x) noexcept
+    { auto y {modint<M>{x}}; return (*this*=y); }
+
+    [[nodiscard]] friend constexpr modint operator+(const modint& lhs, const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(lhs.value_)+static_cast<int64_t>(rhs.value_))%M}; }
+
+    [[nodiscard]] friend constexpr modint operator+(int64_t x, const modint& rhs) noexcept
+    { return modint{x}+rhs; }
+
+    [[nodiscard]] friend constexpr modint operator+(const modint& lhs, int64_t x) noexcept
+    { return lhs+modint{x}; }
+
+    [[nodiscard]] friend constexpr modint operator-(const modint& lhs, const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(lhs.value_)-static_cast<int64_t>(rhs.value_)+M)%M}; }
+
+    [[nodiscard]] friend constexpr modint operator-(int64_t x, const modint& rhs) noexcept
+    { return modint{x}-rhs; }
+
+    [[nodiscard]] friend constexpr modint operator-(const modint& lhs, int64_t x) noexcept
+    { return lhs-modint{x}; }
+
+    [[nodiscard]] friend constexpr modint operator*(const modint& lhs, const modint& rhs) noexcept
+    { return modint{(static_cast<int64_t>(lhs.value_)*static_cast<int64_t>(rhs.value_))%M}; }
+
+    [[nodiscard]] friend constexpr modint operator*(int64_t x, const modint& rhs) noexcept
+    { return modint{x}*rhs; }
+
+    [[nodiscard]] friend constexpr modint operator*(const modint& lhs, int64_t x) noexcept
+    { return lhs*modint{x}; }
+
+    [[nodiscard]] friend constexpr bool operator==(const modint& lhs, const modint& rhs) noexcept
+    { return lhs.value_ == rhs.value_; }
+
+    [[nodiscard]] friend constexpr bool operator!=(const modint& lhs, const modint& rhs) noexcept
+    { return !(lhs == rhs); }
+
+    [[nodiscard]] constexpr int value() const noexcept 
+    { return value_; }
+
+    [[nodiscard]] constexpr modint inverse() noexcept
+    { return bin_pow(*this, M-2); }
 
 private:
-    vector<int64_t> p;
-    const int64_t m {};
+    int value_ {};
+};
+
+template<int M, int P>
+struct rolling_hash_powers {
+    explicit rolling_hash_powers(const std::size_t size)
+        :p(size), p_inv(size)
+    {
+        p[0] = mint{1};
+        p_inv[0] = p[0].inverse();
+        for (std::size_t i{1}; i<size; ++i) {
+            p[i] = p[i-1]*P;
+            p_inv[i] = p[i].inverse();
+        }
+    }
+
+    std::vector<peria::modint<M>> p;
+    std::vector<peria::modint<M>> p_inv;
+};
+
+// polynomial rolling hash
+struct rolling_hash {
+    template<int M, int P>
+    rolling_hash(const std::string& s, const rolling_hash_powers<M, P>& powers)
+        :h(s.size()+1)
+    {
+        for (std::size_t i{}; i<s.size(); ++i) {
+            h[i+1] = h[i] + (powers.p[i]*char_ascii_value(s[i]));
+        }
+    }
+
+    [[nodiscard]]
+    static int char_ascii_value(unsigned char character) noexcept
+    { return static_cast<int>(character)+1; }
+
+    // range indices are 1-based
+    template<int M, int P>
+    [[nodiscard]] int sub_hash(std::size_t i, std::size_t j, const rolling_hash_powers<M, P>& powers)
+    { return ((h[j]-h[i-1])*powers.p_inv[i-1]).value(); }
+
+    [[nodiscard]] int hash() const
+    { return h.back().value(); }
+
+private:
+    std::vector<mint> h; // 1-indexed
+};
+
+}
+
+struct dup_info {
+    std::string s;
+    int count {};
 };
 
 // search for duplicate strings in a list of strings.
 [[nodiscard]]
-vector<pair<string, int>> count_duplicates(const vector<string>& strs, const rolling_hash& rh)
+std::vector<dup_info> count_duplicates(const std::vector<std::string>& strs)
 {
+    const peria::rolling_hash_powers<peria::mint::MOD, 31> rhp {10000}; // only using lowercase english letters, hence 31 as P
     const int n {static_cast<int>(strs.size())};
 
-    vector<pair<int64_t, std::size_t>> hashes; hashes.reserve(n);
+    //                                 hash   string's index 
+    std::vector<std::pair<peria::rolling_hash, std::size_t>> hashes; hashes.reserve(n);
     for (std::size_t i{}; i<strs.size(); ++i) {
-        hashes.emplace_back(make_pair(rh.compute_hash(strs[i]), i));
+        hashes.emplace_back(std::make_pair(peria::rolling_hash{strs[i], rhp}, i));
     }
 
-    // pair sorts by first (by default)
-    sort(hashes.begin(), hashes.end()); // Don't sort strs, because we string comparison by default will be linear, so we will get
-                                        // time complexity of O(n*log(n)*m), where n is length of list, and m is the length of largest string.
-                                        // By getting their hashes, and sorting on hashes we remove extra (*m).
-                                        // So we get O(n*m + n*log(n)) --> nlogn for sorting and n*m for hash precomputation (for iterating over each string).
+    // Don't sort strs, because string comparison by default will be linear, so we will get
+    // time complexity of O(n*log(n)*m), where n is length of list, and m is the length of largest string.
+    // By getting their hashes, and sorting on hashes we remove extra (*m).
+    // So we get O(n*m + n*log(n)) --> nlogn for sorting and n*m for hash precomputation (for iterating over each string).
+    sort(hashes.begin(), hashes.end(), [](const auto& a, const auto& b) { return a.first.hash() < b.first.hash(); }); 
 
-    vector<pair<string, int>> ans; ans.reserve(256);
+    std::vector<dup_info> ans; ans.reserve(256);
     for (int i{1}; i<n; ++i) {
-        if (hashes[i].first == hashes[i-1].first) {
-            ans.emplace_back(make_pair(strs[hashes[i].second], 1));
-            while (i<n && hashes[i].first == hashes[i-1].first) {
-                ++ans.back().second;
+        if (hashes[i].first.hash() == hashes[i-1].first.hash()) {
+            ans.emplace_back(dup_info{strs[hashes[i].second], 1});
+            while (i<n && hashes[i].first.hash() == hashes[i-1].first.hash()) {
+                ++ans.back().count;
                 ++i;
             }
         }
@@ -88,17 +220,15 @@ vector<pair<string, int>> count_duplicates(const vector<string>& strs, const rol
 
 int main() 
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    rolling_hash rh {static_cast<size_t>(5e5), 31, static_cast<int64_t>(1e9+9)};
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     
-    vector<string> strs {"periaraperi", "laga", "laggert", "abc", "abacaba", "abc", 
-                         "laga", "peria", "poyera", "abc", "laga", "poyera", "daba", "baba", "caba"};
+    std::vector<std::string> strs {"periaraperi", "laga", "laggert", "abc", "abacaba", "abc", 
+                                   "laga", "peria", "poyera", "abc", "laga", "poyera", "daba", "baba", "caba"};
 
-    const auto dups {count_duplicates(strs, rh)};
+    const auto dups {count_duplicates(strs)};
     for (const auto& [s, count]:dups) {
-        cout << s << " - " << count << '\n';
+        std::cout << s << " - " << count << '\n';
     }
     
     return 0;
